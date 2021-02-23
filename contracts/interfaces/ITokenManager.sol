d4ec81b1027093a1aa05a9e905c65766e5cd1516
0x112358 <gilfoyleproton@protonmail.com>
2021-02-23 13:57:35 +0300

    added more methods

--- contracts/interfaces/ITokenManager.sol
+++ contracts/interfaces/ITokenManager.sol
@@ -15,6 +15,14 @@ interface ITokenManager {
         view
         returns (bool);
 
+    /// Checks if the address is token admin
+    /// @param admin The address of the potential admin to check
+    /// @return True if address is admin
+    function isTokenAdmin(address admin)
+        external
+        view
+        returns (bool);
+
     /// Address of the underlying token
     /// @param syntheticTokenAddress The address of the synthetic token
     function underlyingToken(address syntheticTokenAddress)
